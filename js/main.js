a95018ff0bff2066ba75c408b799441f3d053684
Sam Smith <sam@Sams-iMac.local>
2014-12-02 12:09:45 +0000

    thank you message

--- js/main.js
+++ js/main.js
@@ -229,4 +229,23 @@ $(document).ready(function() {
     targets: '.navigation, .banner .text',
     images: '.banner'
   });
+  
+  
+  //contact 
+  function getQueryVariable(variable) {
+     var query = window.location.search.substring(1);
+     var vars = query.split("&");
+     for (var i=0;i<vars.length;i++) {
+             var pair = vars[i].split("=");
+             if(pair[0] == variable){return pair[1];}
+     }
+     return(false);
+  }
+  
+  var form = getQueryVariable('form');
+  
+  if (form === 'sent') {
+    $('.contact .modal').addClass('modal-active');
+  }
+
 });
